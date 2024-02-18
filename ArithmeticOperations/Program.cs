a5dcae7a62e7f719e726d47b2c21336026156363
Radu Robert <radurobert1234@yahoo.com>
2024-02-18 02:41:04 +0200

    create a function to add the numbers

--- ArithmeticOperations/Program.cs
+++ ArithmeticOperations/Program.cs
@@ -3,3 +3,11 @@
     int firstNumberToAdd = int.Parse(Console.ReadLine());
     int secondNumberToAdd = int.Parse(Console.ReadLine());
 }
+
+static int AddNumbers(int firstNumber, int secondNumber)
+{
+    int result;
+    result = firstNumber + secondNumber;
+    return result;
+}
+
