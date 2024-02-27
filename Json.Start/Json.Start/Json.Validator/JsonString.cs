402b0cddbcde9e6687a1a571ba2dbeac685d2d6d
Radu Robert <radurobert1234@yahoo.com>
2024-02-27 03:18:33 +0200

    functia verifica daca exista caractere de control

--- Json.Start/Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Start/Json.Validator/JsonString.cs
@@ -12,9 +12,22 @@ namespace Json
                 return false;
             }
 
+            foreach (char c in input)
+            {
+                if (IsControlCharacter(c))
+                {
+                    return false;
+                }
+            }
+
             result = (input[0] == '"') && (input[input.Length - 1] == '"');
 
             return result;
+        }
+
+        static bool IsControlCharacter(char c)
+        {
+            return c == '\n' || c == '\r';
         }
     }
 }
