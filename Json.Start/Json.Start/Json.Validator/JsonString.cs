3914ab62af684083c41c1eb04b3bc5c5a7d74744
Radu Robert <radurobert1234@yahoo.com>
2024-03-18 13:49:51 +0200

    jump with the index over the escape sequence if its valid

--- Json.Start/Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Start/Json.Validator/JsonString.cs
@@ -16,11 +16,13 @@ namespace Json
                 return false;
             }
 
-            for (int i = 0; i < input.Length - 1; i++)
+            int step = 1;
+            for (int i = 0; i < input.Length - 1; i += step)
             {
+                step = 1;
                 char currentChar = input[i];
                 char nextChar = input[i + 1];
-                if (currentChar == '\\' && !HandleAllEscapeSequences(nextChar, input, i))
+                if (currentChar == '\\' && !HandleAllEscapeSequences(nextChar, input, i, ref step))
                 {
                     return false;
                 }
@@ -53,43 +55,40 @@ namespace Json
             return countQuote % isPair == 0;
         }
 
-        static bool HandleAllEscapeSequences(char charToCheck, string input, int index)
+        static bool HandleAllEscapeSequences(char charToCheck, string input, int index, ref int step)
         {
             int nextCharIndex = index + 1;
-            return charToCheck switch
+            const int charsToSkipIfIsUnicode = 4;
+            if (charToCheck == 'u' && IsValidUnicodeSequence(input, nextCharIndex))
             {
-                'u' => CheckUnicodeValidity(input, nextCharIndex),
-                '"' => !IsLastCharacter(input, nextCharIndex),
-                _ => IsEscapeSequence(input, index, charToCheck)
+                step += charsToSkipIfIsUnicode;
+                return true;
+            }
+            else if (charToCheck == '"' && !IsLastCharacter(input, nextCharIndex))
+            {
+                step++;
+                return true;
+            }
+            else
+            {
+                if (IsCharacterEscape(charToCheck))
+                {
+                    step++;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static bool IsCharacterEscape(char c)
+        {
+            return c switch
+            {
+                'f' or 'n' or 'r' or '/' => true,
+                'b' or 't' or '\\' => true,
+                _ => false
             };
-        }
-
-        static bool IsEscapeSequence(string input, int index, char c)
-        {
-            if (c == 'f' || c == 'n' || c == 'r')
-            {
-                return true;
-            }
-            else if (c == '/' || c == 'b' || c == 't' || IsValidBackslash(input, index, c))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        static bool IsValidBackslash(string input, int index, char c)
-        {
-            if (c == '\\')
-            {
-                return true;
-            }
-            else if (input[index - 1] == '\\')
-            {
-                return true;
-            }
-
-            return false;
         }
 
         static bool IsLastCharacter(string input, int nextCharIndex)
@@ -97,7 +96,7 @@ namespace Json
             return nextCharIndex == input.Length - 1;
         }
 
-        static bool CheckUnicodeValidity(string input, int nextCharIndex)
+        static bool IsValidUnicodeSequence(string input, int nextCharIndex)
         {
             const int unicodeCharactersToCheck = 5;
             for (int j = 1; j < unicodeCharactersToCheck; j++)
