1d8c34adb8e9c317883238dd7f6e9a4e6656e048
Radu Robert <radurobert1234@yahoo.com>
2024-04-22 14:28:39 +0300

    refactoring tests

---
+++ oop/OOP/Any.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP
+{
+    public class Any : IPattern
+    {
+        public Any(string accepted)
+        {
+        }
+
+        public IMatch Match(string text)
+        {
+            return new Match(false, text);
+        }
+    }
+}
---
+++ oop/OOPTests/AnyFacts.cs
@@ -0,0 +1,29 @@
+﻿using OOP;
+
+namespace OOPTests
+{
+    public class AnyFacts
+    {
+        [Fact]
+        public void Match_Should_ReturnTrue_WhenStartingCharacterIsValid()
+        {
+            var e = new Any("eE");
+            Assert.True(e.Match("ea").Success());
+            Assert.True(e.Match("Ea").Success());
+        }
+        [Fact]
+        public void Match_Should_ReturnFalse_WhenStartingCharacterIsNotValid()
+        {
+            var e = new Any("eE");
+            Assert.False(e.Match("a").Success());
+        }
+
+        [Fact]
+        public void Match_Should_ReturnFalse_WhenValueIsEmptyOrNull()
+        {
+            var e = new Any("eE");
+            Assert.False(e.Match("").Success());
+            Assert.False(e.Match(null).Success());
+        }
+    }
+}
