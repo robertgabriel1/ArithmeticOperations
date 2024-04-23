d95841d8d466967c62c21ff46e44be709383ec57
Radu Robert <radurobert1234@yahoo.com>
2024-04-23 22:09:11 +0300

    added class many and tests

---
+++ oop/OOP/Many.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP
+{
+    public class Many : IPattern
+    {
+        private readonly IPattern pattern;
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            var match = pattern.Match(text);
+            do
+            {
+                text = match.RemainingText();
+                match = pattern.Match(text);
+            } while (match.Success());
+
+            return new Match(true, text);
+        }
+    }
+}
---
+++ oop/OOPTests/ManyFacts.cs
@@ -0,0 +1,51 @@
+﻿using OOP;
+
+namespace OOPTests
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void Match_Should_RemoveSimilarCharactersFromBeginning_WhenTextStartsWithPattern()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("abc").Success());
+            Assert.True(a.Match("aaaabc").Success());
+        }
+
+        [Fact]
+        public void Match_Should_ReturnSameText_WhenPatternCharIsAtAnotherPositionThanTheFirstOne()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("bac").Success());
+        }
+
+        [Fact]
+        public void Match_Should_ReturnSameText_WhenTextDoesNotStartsWithPattern()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("bc").Success());
+        }
+
+        [Fact]
+        public void Match_Should_ReturnSameText_WhenTextIsEmptyOrNull()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("").Success());
+            Assert.True(a.Match(null).Success());
+        }
+
+        [Fact]
+        public void Match_Should_RemoveSimilarCharactersFromBeginning_WhenFirstCharactersAreInRange()
+        {
+            var digits = new Many(new OOP.Range('0', '9'));
+            Assert.True(digits.Match("12345ab123").Success());
+        }
+
+        [Fact]
+        public void Match_Should_ReturnSameText_WhenFirstCharactersAreNotInRange()
+        {
+            var digits = new Many(new OOP.Range('0', '9'));
+            Assert.True(digits.Match("ab").Success());
+        }
+    }
+}
